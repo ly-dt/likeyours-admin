c41d325aa718593f363fe4b7f46e47d7ac50d38d
unknown <johnleoclaudio@gmail.com>
2019-12-10 23:30:45 +0800

    updated repo

--- src/repository/index.js
+++ src/repository/index.js
@@ -3,6 +3,6 @@ import * as UsersRepo from './users';
 import * as CategoriesRepo from './categories';
 import * as RentablesRepo from './rentables';
 
-const BASE_URL = 'http://52.77.247.81:3000';
+const BASE_URL = 'http://13.251.157.35:3000';
 
 export { BASE_URL, StationsRepo, UsersRepo, RentablesRepo, CategoriesRepo };
--- src/repository/users/getUsers.js
+++ src/repository/users/getUsers.js
@@ -4,6 +4,6 @@ import dataFormatterForTables from '../../utils/dataFormatterForTables';
 
 export default async function getUsers() {
   const response = await axios.get(`${BASE_URL}/users`);
-  const formattedData = dataFormatterForTables(response.data.data);
+  const formattedData = dataFormatterForTables(response.data.users);
   return formattedData;
 }
